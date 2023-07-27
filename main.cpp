1f797d1eea543d111c79363a69039f34099e1455
AHZHONG <leolai0224@gmail.com>
2023-07-27 17:35:28 +0800

    Finishing Game looping

--- main.cpp
+++ main.cpp
@@ -4,24 +4,27 @@
 using namespace std;
 
 int main() {
-    Chess chess;
-    string input;
-    bool condition;
-    do {
-        cout << "Menu" << endl;
-        cout << "Press P to play. Press Q to leave game" << endl;
-        getline(cin, input, '\n');
-        condition = (input[0] == 'P' || input[0] == 'Q' || input[0] == 'p' || input[0] == 'q' || input.length() == 1);
-        if (condition) {
-            break;
+    while (true) {
+        Chess chess;
+        string input;
+        bool condition;
+        do {
+            cout << "Menu" << endl;
+            cout << "Press P to play. Press Q to leave game" << endl;
+            getline(cin, input, '\n');
+            condition = (input[0] == 'P' || input[0] == 'Q' || input[0] == 'p' || input[0] == 'q' || input.length() == 1);
+            if (condition) {
+                break;
+            }
+        } while (!condition);
+        input[0] = toupper(input[0]);
+        if (input[0] == 'P') {
+            chess.start();
         }
-    } while (!condition);
-    input[0] = toupper(input[0]);
-    if (input[0] == 'P') {
-        chess.start();
+        if (input[0] == 'Q') {
+            break;
+        }  
     }
-    if (input[0] == 'Q') {
-        return 0;
-    }  
-    return -1;
+    
+    return 0;
 }
